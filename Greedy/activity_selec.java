d53aaa18e63314e850adcb6482cacc021b1873f9
Anisha100 <87559560+Anisha100@users.noreply.github.com>
2022-06-04 09:30:10 +0530

    Update activity_selec.java

--- Greedy/activity_selec.java
+++ Greedy/activity_selec.java
@@ -1,1 +1,16 @@
-
+ i = 0;
+    System.out.print(i+" ");
+      
+    // Consider rest of the activities
+    for (j = 1; j < n; j++)
+    {
+         // If this activity has start time greater than or
+         // equal to the finish time of previously selected
+         // activity, then select it
+         if (s[j] >= f[i])
+         {
+              System.out.print(j+" ");
+              i = j;
+          }
+     }
+    }
