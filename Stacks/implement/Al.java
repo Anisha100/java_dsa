8197c0728960e9962c0bd410c3499bf28f7c81c2
Anisha100 <87559560+Anisha100@users.noreply.github.com>
2022-06-11 07:14:30 +0530

    Update Al.java

--- Stacks/implement/Al.java
+++ Stacks/implement/Al.java
@@ -1,1 +1,36 @@
-
+import java.util.*;
+public class Main
+{
+ArrayList<Integer> list=new ArrayList<>();
+public boolean isnull()
+{
+    return list.size()==0;
+    
+}
+public void push(int d)
+{
+    list.add(d);
+    
+}
+public int pop(){
+    int val=list.get(list.size()-1);
+    list.remove(list.size()-1);
+    return val;
+}
+public int peek(){
+    return list.get(list.size()-1);
+    
+    
+}
+   public static void main(String args[])
+    {
+        Stack s = new Stack();
+        s.push(10);
+        s.push(20);
+        s.push(30);
+        System.out.println(s.pop() + " Popped from stack");
+        System.out.println("Top element is :" + s.peek());
+        System.out.print("Elements present in stack :");
+     
+    }
+}
